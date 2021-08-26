c8d86e2a4ffaa9e5c92c50f89cae459d217b207b
GasparAmato <gasparamato0304@gmail.com>
2021-08-25 23:23:50 -0300

    proyecto final

--- src/componentes/Cart/InfoAndPurchase.jsx
+++ src/componentes/Cart/InfoAndPurchase.jsx
@@ -10,11 +10,36 @@ const InfoAndPurchase = () => {
 const { carrito, totalPrice, clearCart} = useContext(CartContext)
 console.log(carrito)
 
+// validation states
+const [name, setName] = useState("")
+const [name2, setName2] = useState("")
+const [phone, setPhone] = useState(0)
+const [phone2, setPhone2] = useState(0)
+const [mail, setMail] = useState("")
+const [mail2, setMail2] = useState("")
+
+const [buttonAvailability, setButtonAvailability] = useState(false)
+
+useEffect(() => {
+    if(name === name2 & phone === phone2 & mail === mail2 ){
+        if(name === "" || phone === 0 || mail === ""){
+            setButtonAvailability(false)
+        }else{
+        setButtonAvailability(true)
+        console.log("funciono")}
+    } else{
+        setButtonAvailability(false)
+        console.log("no funciono")     
+    }
+}, [name, name2, phone, phone2, mail, mail2])
+
+
 const [buyer, setBuyer] = useState({
     name:"", 
     phone:"", 
     email:""
 })
+
 const [activado, setActivado] = useState(true)
 
 
@@ -23,15 +48,15 @@ const [activado, setActivado] = useState(true)
     const hanldleInput = (event) => {
         console.log(event.target.value)
         setBuyer({
-            ...buyer,
-            [event.target.name] : event.target.value
+            ...buyer, [event.target.name] : event.target.value
         })
-        console.log("holad")
+        console.log("buyer 1")
         console.log(buyer)
         
 
 
     }
+    
     
 
 
@@ -71,6 +96,8 @@ const [activado, setActivado] = useState(true)
         sendBuyer(buyer, items, total)
     }
 
+
+
     const [id, setId] = useState([])
     useEffect(() => {
     cartCollection.get().then(query => setId(query.docs.map( (u) => u.id)) )
@@ -98,14 +125,18 @@ const [activado, setActivado] = useState(true)
 
         <form className="row" onSubmit={handleOnClick}>
             <h2>Formulario</h2>
-           
+  <div className="row">         
 <div className="col-md-3">                
   <input 
     type="text" 
     className="form-control" 
     placeholder="enter name" 
     name="name"
-    onChange={hanldleInput}>
+    onChange={hanldleInput}
+    // eslint-disable-next-line
+    onChange={(event)=> setName(event.target.value)}>
+
+
   </input>
 </div>
 
@@ -115,7 +146,9 @@ type="number"
 className="form-control"
 placeholder="Phone Number"
  name="phone"
- onChange={hanldleInput}>      
+ onChange={hanldleInput}
+ // eslint-disable-next-line
+ onChange={(e)=>setPhone(e.target.value)}>      
   </input>
 </div>
 
@@ -125,18 +158,67 @@ type="email"
 className="form-control" 
 placeholder="e-mail" 
 name="email" 
-onChange={hanldleInput}>
+onChange={hanldleInput}
+// eslint-disable-next-line
+onChange={(e)=> setMail(e.target.value)}
+>
 </input>
 </div>
 
 
 
+</div>
+        
+
+{/*-------- VALIDACION -------*/}
+
+        
+            
+  <div className="row">         
+<div className="col-md-3">                
+  <input 
+    type="text" 
+    className="form-control" 
+    placeholder="enter name" 
+    name="name"
+    onChange={(e) => setName2(e.target.value)}>
+  </input>
+</div>
+
+<div className="col-md-3"> 
+<input 
+type="number" 
+className="form-control"
+placeholder="Phone Number"
+ name="phone"
+ onChange={(e) => setPhone2(e.target.value)}>      
+  </input>
+</div>
+
+<div className="col-md-3"> 
+<input 
+type="email" 
+className="form-control" 
+placeholder="e-mail" 
+name="email" 
+onChange={(e) => setMail2(e.target.value)}>
+</input>
+</div>
+</div>
+
+{ buttonAvailability &&
             <div className="col-md-3">
-                
-<button className="btn btn-primary" type="submit" >make order</button>
-                
+   
+<button  className="btn btn-primary" type="submit" >make order</button>
+              
             </div>
+             } 
+{ !buttonAvailability &&
+        <div>las casillas de validacion no coinciden o estan vacias </div>
+
+             }
         </form>
+
 
 <h3>purchase info:</h3>
 {carrito.map((e) => {
